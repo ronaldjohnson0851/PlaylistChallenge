44b9b8aee77d26b76a275f6ef5275659a63126ba
rjohnson0851 <ron21johnson@comcast.net>
2024-11-06 20:13:48 -0500

    Changes

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,18 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+            startIndex = 1;
+            int jumpsForward = 0;
+            for (int i = startIndex; i < playList.length; i++) {
+                if (playList[i] != selection) {
+                    jumpsForward += 1;
+                } return jumpsForward;
+            }
+            return jumpsForward - playList.length;
     }
 }
